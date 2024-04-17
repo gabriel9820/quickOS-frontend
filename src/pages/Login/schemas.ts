b4c3ff240bfa68173ccca507261e4278048623d1
Gabriel Alves <gabriel_alves_44@live.com>
2024-04-17 18:34:37 -0300

    refactor: login form schema

--- src/pages/Login/schemas.ts
+++ src/pages/Login/schemas.ts
@@ -2,6 +2,7 @@ import z from "zod";
 
 export const loginFormSchema = z.object({
   email: z.string().email("Email inválido").min(1, "Email é obrigatório"),
+  password: z.string(),
 });
 
 export type LoginFormData = z.infer<typeof loginFormSchema>;
