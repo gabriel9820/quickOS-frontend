670377047dec93d665d48ffa41d1754b33c3574d
Gabriel Alves <gabriel_alves_44@live.com>
2024-05-06 15:49:08 -0300

    feat: tab form

---
+++ src/components/Form/Tabs.tsx
@@ -0,0 +1,68 @@
+import { Box, Button, Tab, TabProps } from "@mui/material";
+import {
+  LoadingButton,
+  TabList,
+  TabListProps,
+  TabPanel,
+  TabPanelProps,
+} from "@mui/lab";
+import { Save } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+
+export function FormTabList({ children, ...props }: TabListProps) {
+  return (
+    <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+      <TabList {...props}>{children}</TabList>
+    </Box>
+  );
+}
+
+export function FormTab({ children, ...props }: TabProps) {
+  return <Tab {...props}>{children}</Tab>;
+}
+
+export function FormTabPanel({ children, ...props }: TabPanelProps) {
+  return <TabPanel {...props}>{children}</TabPanel>;
+}
+
+export interface FormTabActionsProps {
+  readOnly: boolean;
+  loading?: boolean;
+}
+
+export function FormTabActions({ readOnly, loading }: FormTabActionsProps) {
+  const navigate = useNavigate();
+
+  function handleCancelClick() {
+    navigate(-1);
+  }
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "end",
+        borderTop: 1,
+        borderColor: "divider",
+        paddingTop: 3,
+      }}
+    >
+      <Button variant="outlined" color="secondary" onClick={handleCancelClick}>
+        {readOnly ? "Voltar" : "Cancelar"}
+      </Button>
+
+      {!readOnly && (
+        <LoadingButton
+          variant="contained"
+          color="primary"
+          startIcon={<Save />}
+          loading={loading}
+          type="submit"
+          sx={{ marginLeft: 1 }}
+        >
+          Salvar
+        </LoadingButton>
+      )}
+    </Box>
+  );
+}
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,8 +1,22 @@
 import { PropsWithChildren } from "react";
 import { FieldValues, FormProvider, UseFormReturn } from "react-hook-form";
+import {
+  TabContext,
+  TabContextProps,
+  TabListProps,
+  TabPanelProps,
+} from "@mui/lab";
+import { TabProps } from "@mui/material";
 
 import { UField, UFieldProps } from "./UField";
 import { CField, CFieldProps } from "./CField";
+import {
+  FormTab,
+  FormTabActions,
+  FormTabActionsProps,
+  FormTabList,
+  FormTabPanel,
+} from "./Tabs";
 
 interface Props<T extends FieldValues> extends PropsWithChildren {
   form: UseFormReturn<T>;
@@ -30,5 +44,11 @@ Form.defaultProps = {
 
 Form.UField = (props: UFieldProps) => UField(props);
 Form.CField = (props: CFieldProps) => CField(props);
+
+Form.TabContext = (props: TabContextProps) => TabContext(props);
+Form.TabList = (props: TabListProps) => FormTabList(props);
+Form.Tab = (props: TabProps) => FormTab(props);
+Form.TabPanel = (props: TabPanelProps) => FormTabPanel(props);
+Form.TabActions = (props: FormTabActionsProps) => FormTabActions(props);
 
 export { Form };
