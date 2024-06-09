cca127fb1b62db5da9207f958ac26efc294f2bda
Gabriel Alves <gabriel_alves_44@live.com>
2024-06-09 14:50:09 -0300

    refactor: customer columns

--- src/pages/Customers/List/constants.tsx
+++ src/pages/Customers/List/constants.tsx
@@ -2,9 +2,20 @@ import { GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
 
 import { StatusChip } from "../../../components/DataTable/StatusChip";
 import { CustomerOutputModel } from "../../../models/customer.model";
+import { customerTypeOptions } from "../../../components/Autocomplete/CustomerTypeAutocomplete";
+import { CustomerType } from "../../../enums/customer-type.enum";
 
 export const columns: GridColDef[] = [
+  { field: "code", headerName: "Código", width: 150 },
   { field: "fullName", headerName: "Nome", flex: 1 },
+  {
+    field: "type",
+    headerName: "Tipo de Pessoa",
+    width: 150,
+    valueFormatter: (value: CustomerType) =>
+      customerTypeOptions.find((o) => o.key === value)?.label,
+  },
+  { field: "document", headerName: "Documento", width: 150 },
   {
     field: "isActive",
     headerName: "Status",
